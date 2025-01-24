5026146c5c8f108e0ef269f55a78577b42a40e76
ramenrolls <ramenrolls@proton.me>
2025-01-24 14:12:21 +0100

    gta3xlwifi: Import Bluetooth Stack

--- device.mk
+++ device.mk
@@ -2,6 +2,18 @@
 # SPDX-FileCopyrightText: 2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1-service \
+    android.hardware.bluetooth.audio-impl \
+    audio.bluetooth.default \
+    libbt-vendor
+
+PRODUCT_COPY_FILES += \
+    hardware/samsung_slsi/libbt/conf/bt_did.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_did.conf \
+    hardware/samsung_slsi/libbt/conf/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
