e47a0a05821cfc4840a5d6b2fdea28ca2dcc520a
ramenrolls <ramenrolls@proton.me>
2025-01-24 14:01:39 +0100

    gta3xlwifi: Declare device platform and hardware identifiers
    * platform: Exynos 7885/7904
    * define BOARD_VENDOR and TARGET_SOC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,5 +4,17 @@
 #
 DEVICE_PATH := device/samsung/gta3xlwifi
 
+# Platform
+TARGET_BOARD_PLATFORM := universal7904
+BOARD_VENDOR := Samsung
+TARGET_SOC := exynos7904
+TARGET_BOOTLOADER_BOARD_NAME := universal7904
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit the proprietary files
 include vendor/samsung/gta3xlwifi/BoardConfigVendor.mk
